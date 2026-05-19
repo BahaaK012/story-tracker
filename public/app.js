6e9ce395832d729e1e70fa7d5e977f210741cff8
Bahaa <bbah2334@gmail.com>
2026-05-19 22:00:54 +0300

    updated theme now I like it I guess

--- public/app.js
+++ public/app.js
@@ -37,3 +37,22 @@ function router() {
 //listeners for SPA navigation
 window.addEventListener('hashchange', router);
 window.addEventListener('DOMContentLoaded', router);
+
+// Theme Switcher Logic
+const themeBtn = document.getElementById('theme-toggle');
+
+// Check local storage for saved theme
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('theme-light');
+}
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('theme-light');
+    
+    // Save preference so it remembers when you refresh
+    if (document.body.classList.contains('theme-light')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
